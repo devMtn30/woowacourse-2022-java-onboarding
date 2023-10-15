a868010d4286ca355b7668aa54982916013b6632
DESKTOP-A7QDIKE\tomato <devmtn30@gmail.com>
2023-10-15 12:48:17 +0900

    - problem6

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,30 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        Set<String> invalidNicknames = new HashSet<>();
+
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            String nickname = form.get(1);
+
+            for (int i = 0; i < forms.size(); i++) {
+                List<String> otherForm = forms.get(i);
+                if (!otherForm.get(0).equals(email)) {
+                    for (int j = 0; j < nickname.length() - 1; j++) {
+                        if(otherForm.get(1).contains(nickname.substring(j,j+2))) {
+                            invalidNicknames.add(otherForm.get(0));
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        List<String> result = new ArrayList<>(invalidNicknames);
+        Collections.sort(result);
+        return result;
     }
 }
