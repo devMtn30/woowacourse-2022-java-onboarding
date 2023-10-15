8f438d3bc68f085f613526b8c4a65870ee09ab6b
DESKTOP-A7QDIKE\tomato <devmtn30@gmail.com>
2023-10-15 13:13:01 +0900

    - problem 7

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,44 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem7 {
+    // 친구 추천 점수를 저장할 딕셔너리
+    static Map<String, Integer> friendScoreDict = new HashMap<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        List<String> userFriends = new ArrayList<>();
+
+        for (List<String> friendRelations : friends) {
+            if (friendRelations.get(0).equals(user)) userFriends.add(friendRelations.get(1));
+            else if (friendRelations.get(1).equals(user)) userFriends.add(friendRelations.get(0));
+        }
+
+        for (List<String> friendRelations : friends) {
+            String userA = friendRelations.get(0);
+            String userB = friendRelations.get(1);
+            if (userA.equals(user) || userB.equals(user)) continue;
+            if (userFriends.contains(userA) && !userFriends.contains(userB)) {
+                putScoreDict(userB, 10);
+            } else if (userFriends.contains(userB) && !userFriends.contains(userA)) {
+                putScoreDict(userA, 10);
+            }
+        }
+
+        for (String visitor : visitors) {
+            if (!userFriends.contains(visitor)) {
+                putScoreDict(visitor, 1);
+            }
+        }
+
+        return friendScoreDict.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed().thenComparing(Map.Entry::getKey))
+                .limit(5)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
+    private static void putScoreDict(String name, int score) {
+        friendScoreDict.put(name, friendScoreDict.getOrDefault(name, 0) + score);
     }
 }
